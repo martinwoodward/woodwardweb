7538b278a577a3f8dc71a771da7dc21108620883
Martin Woodward <martinwoodward@github.com>
2025-08-05 22:39:42 +0100

    Refactor Astro configuration to include AutoImport and MDX integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,31 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import AutoImport from 'astro-auto-import';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://woodwardweb.com',
-  integrations: [react()],
+  integrations: [
+    AutoImport({
+      imports: [
+        './src/layouts/shortcodes/Button.tsx',
+        './src/layouts/shortcodes/Notice.tsx',
+        './src/layouts/shortcodes/Accordion.tsx',
+        './src/layouts/shortcodes/Video.tsx',
+        './src/layouts/shortcodes/Youtube.tsx',
+        './src/layouts/shortcodes/Tabs.tsx',
+        './src/layouts/shortcodes/Tab.tsx'
+      ]
+    }),
+    react(),
+    mdx()
+  ],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    ssr: {
+      noExternal: ['react-lite-youtube-embed']
+    }
   }
 });
